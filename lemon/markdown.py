0eb87808feece9ab7c37133f3cb61096de1b4d1c
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-05-18 01:07:26 +0000

    Added recursive rendering

--- lemon/markdown.py
+++ lemon/markdown.py
@@ -1,15 +1,20 @@
 import typing as t
+from collections import abc
 
 
 class Markdown:
-    pass
+    def render(self, *_, **__) -> "Renderable":
+        yield ""
 
 
 MarkdownType = t.Union[Markdown, str]
+Renderable = t.Union[MarkdownType, t.Iterable[MarkdownType]]
 
 
-class Header(Markdown):
-    def __init__(self, name: str, body: Markdown, depth: int = 0):
-        self.name = name
-        self.content = content
-        self.depth = depth
+def render(content: Renderable, *args, **kwargs) -> str:
+    if isinstance(content, Markdown):
+        return render(content.render(*args, **kwargs), *args, **kwargs)
+    if isinstance(content, str):
+        return content
+    if isinstance(content, abc.Iterable):
+        return "\n\n".join([render(item, *args, **kwargs) for item in content])
