952d1f90147be9ebb03af795f2f4d336974f2d4b
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-05-19 04:35:51 +0000

    Added tables example output

--- examples/table.py
+++ examples/table.py
@@ -9,4 +9,14 @@ data = Table(
 )
 
 print(content := dumps(data))
+"""
+|  Name  |  Location  |  Status  |
+|--------|------------|----------|
+|  Ted  |  New York  |  Busy  |
+|  Angie  |  France  |  Free  |
+"""
+
 print(loads(content))
+"""
+[<Table columns=('Name', 'Location', 'Status') entries=2>]
+"""
