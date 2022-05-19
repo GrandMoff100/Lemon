88defc65b951d327ded7f23267d7a06003012beb
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-05-19 04:37:13 +0000

    Added inline styling example

---
+++ examples/inline_styling.py
@@ -0,0 +1,28 @@
+from lemon import Bold, Italics, Markdown, Strikethrough, dumps, loads
+
+document = (
+    Bold("My bold text"),
+    Italics("is italisized"),
+    "this is not",
+    Strikethrough("and also strikethrough"),
+    "and more regular text",
+    Bold("But bold again"),
+    Strikethrough("and then strikethrough"),
+    Italics("and finally more italics."),
+    "finally some regular text.",
+)
+
+print(content := dumps(document))
+"**My bold text** *is italisized* this is not ~~and also strikethrough~~ and more regular text **But bold again** ~~and then strikethrough~~ *and finally more italics.* finally some regular text. "
+
+print(loads(content))
+"""
+[Markdown(
+    Bold('My bold text'),
+    Italics('is italisized'),
+    'this is not',
+    Strikethrough('and also strikethrough'),
+    'and more regular text',
+    Bold('But bold again'),
+    Strikethrough('and then strikethrough'), Italics('and finally more italics.'), 'finally some regular text.')]
+"""
