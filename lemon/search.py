f546dc9dabc8f42e49c2800b60a04108593f6a04
Nate <minecraftcrusher100@gmail.com>
2022-08-28 22:27:55 +0000

    Add finding by condition

--- lemon/search.py
+++ lemon/search.py
@@ -1,7 +1,11 @@
+from typing import Callable
 from .markdown import Markdown, MarkdownType, Renderable
 
 
 def contains(markdown: Renderable, obj: MarkdownType) -> bool:
+    if isinstance(markdown, (Markdown, list)):
+        if obj in markdown:
+            return True
     if isinstance(markdown, list):
         for item in markdown:
             if contains(item, obj):
@@ -10,7 +14,19 @@ def contains(markdown: Renderable, obj: MarkdownType) -> bool:
         if isinstance(obj, str):
             if obj in markdown:
                 return True
-    if isinstance(markdown, (Markdown, list)):
-        if obj in markdown:
-            return True
     return False
+
+
+def find(markdown: Renderable, condition: Callable[[MarkdownType], bool]) -> list[Renderable]:
+    results = []
+    if isinstance(markdown, list):
+        for item in markdown:
+            result += find(item, condition)
+    if isinstance(markdown, Markdown):
+        results += find(markdown.__children__, condition)
+        if condition(markdown):
+            results.append(markdown)
+    if isinstance(markdown, str):
+        if condition(markdown):
+            results.append(markdown)
+    return results
