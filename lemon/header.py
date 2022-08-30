23ea2494efb600602d7fcf213210875712a0543c
Nate <minecraftcrusher100@gmail.com>
2022-08-30 15:49:15 +0000

    Remove body from header init, add explicit depth

--- lemon/header.py
+++ lemon/header.py
@@ -4,43 +4,40 @@ from .markdown import Markdown, MarkdownType, Renderable
 
 
 class Header(Markdown):
-    __regex__: str = r"\#+\ +(.+)\n((?:.|\n)+)"
+    __regex__: str = r"(\#+)\ *(.+)\n"
 
     def __init__(
         self,
         name: MarkdownType,
-        body: Renderable = "",
+        depth: int = 1,
     ) -> None:
         self.name = name
-        self.body = body
+        self.depth = depth
         super().__init__({})
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}({self.name!r}, {self.body!r})"
+        return f"{self.__class__.__qualname__}({self.name!r}, depth={self.depth!r})"
 
     def __eq__(self, other: t.Any) -> bool:
         if not isinstance(other, Header):
             return False
-        return self.name == other.name and self.body == other.body
+        return self.name == other.name and self.depth == other.depth
 
     @property
-    def __children__(self) -> list[Renderable]:
-        return [self.body]
+    def __children__(self) -> t.List[Renderable]:
+        return [self.name]
 
-    def dumps(self, *args: t.Any, depth: int = 0, **kwargs: t.Any) -> str:
+    def dumps(self, *args: t.Any, **kwargs: t.Any) -> str:
         from .serialize import dumps  # pylint: disable=import-outside-toplevel
 
-        body = dumps(self.body, *args, **kwargs, depth=depth + 1)
-        return f"#{'#' * depth} {dumps(self.name, *args, **kwargs)}{body}"
+        name = dumps(self.name, *args, **kwargs).strip()
+        return f"{'#' * self.depth} {name}\n"
 
     @classmethod
     def loads(  # type: ignore[override]  # pylint: disable=arguments-differ
         cls,
-        _: dict[str, t.Any] | None,
+        _: t.Optional[t.Dict[str, t.Any]],
+        depth: str,
         name: str,
-        body: str,
     ) -> MarkdownType:
-        # Prevents a circular import!
-        from .serialize import loads  # pylint: disable=import-outside-toplevel
-
-        return cls(name=name.strip(), body=loads(body))
+        return cls(name=name.strip(), depth=len(depth))
